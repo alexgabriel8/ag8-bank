d9765b3bc54782ea05ba3861ed7d4601eb08a36e
alexgabriel8 <alexgabriels8@outlook.com>
2023-12-02 20:20:52 -0300

    refactor(addAccount.js): remove reject from call and put resolve() at appropiate line

--- db/scripts/addAccount.js
+++ db/scripts/addAccount.js
@@ -5,26 +5,23 @@ const sqlite3 = importedSqlite3.verbose();
 
 import { dbPath } from "../../constants/dbPath.js"
 
-
 function addAccount(accountName) {
-        return new Promise((resolve, reject) => {
-            const db = new sqlite3.Database(dbPath, (err) => {
-                if(err) console.error(err)
-            })
-        
-            db.run("INSERT INTO accounts (name, balance) values (?, ?)", [accountName, 0], (err) => {
-                if(err) console.error(err)
-            })
-        
-            db.close((err) => {
-                if(err) console.error(err)
-            })
-
-            console.log(chalk.bgGreen.black(`Account "${accountName}" created succesfully`))
-
+    return new Promise(resolve => {
+        const db = new sqlite3.Database(dbPath, (err) => {
+            if(err) console.error(err)
+        })
+    
+        db.run("INSERT INTO accounts (name, balance) values (?, ?)", [accountName, 0], (err) => {
+            if(err) console.error(err)
+        })
+    
+        db.close((err) => {
+            if(err) console.error(err)
+            
+            resolve(console.log(chalk.bgGreen.black(`Account "${accountName}" created succesfully`)))
         })
 
-
+    })
 }
 
 export { addAccount }
